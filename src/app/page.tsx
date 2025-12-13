2995bdd31e0ca8d7ad431730a122bd0bd2f01be7
Anushri <anushritcs@gmail.com>
2025-12-13 13:26:26 +0000

    Remove the suggestion box, make a tree structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,14 @@
 import Header from '@/components/Header';
-import AiSuggestions from '@/components/org-chart/AiSuggestions';
 import OrgNode from '@/components/org-chart/OrgNode';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
-import { orgChartData, projectDetails } from '@/lib/data';
+import { orgChartData } from '@/lib/data';
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen bg-background">
       <Header />
       <main className="flex-1 container mx-auto py-8 px-4 sm:px-6 lg:px-8">
-        <div className="flex flex-col lg:flex-row gap-8 items-start">
-          <div className="w-full lg:w-3/5">
+        <div className="w-full">
             <Card className="overflow-hidden">
               <CardHeader>
                 <CardTitle>Organization Chart</CardTitle>
@@ -26,13 +24,6 @@ export default function Home() {
                 </div>
               </CardContent>
             </Card>
-          </div>
-          <div className="w-full lg:w-2/5">
-            <AiSuggestions
-              currentOrgChart={JSON.stringify(orgChartData, null, 2)}
-              projectDetails={projectDetails}
-            />
-          </div>
         </div>
       </main>
     </div>
